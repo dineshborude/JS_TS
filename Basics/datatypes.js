13d3d67f60dee9b689756ec272b7e2dbcb6e10db
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-20 00:40:53 +0530

    Comeback

---
+++ Basics/datatypes.js
@@ -0,0 +1,10 @@
+
+const accountNumber = 123456789;
+
+let accountMail = 'i4oCt@example.com';
+
+var accountPassword = '123456';
+
+accountCity = 'Kolkata';
+
+console.log(accountCity);
---
+++ Basics/one.js
@@ -0,0 +1,6 @@
+console.log('Comeback to JS');
+console.log('one');
+console.log('two');
+console.log('three');
+console.log('four');
+console.log('five');
