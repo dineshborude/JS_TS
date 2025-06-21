83e406f0daca0c9c1c6e07f0668cbac1109826d8
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-22 01:39:20 +0530

    java conditions

--- Basics/condStatements.js
+++ Basics/condStatements.js
@@ -3,17 +3,37 @@ const readline = require('readline').createInterface({
   output: process.stdout
 });
 
-readline.question('Enter a number ', num => {
-  console.log(`Number :  ${num}!`);
+// readline.question('Enter a number ', num => {
+//   console.log(`Number :  ${num} !`);
 
-    if (num % 5 === 0) {
-    console.log("Multiple Of 5");
-}else {
-    console.log("Not a multiple of 5");
-}
+//     if (num % 5 === 0) {
+//     console.log("Multiple Of 5");
+//     }else {
+//         console.log("Not a multiple of 5");
+//     }
 
-  readline.close();
-});
+//     let tensMultiple = (num % 10 === 0) ? 'Multiple of 10' : 'Not a multiple of 10';
+//     console.log(tensMultiple);
+
+//   readline.close();
+// });
+
+readline.question('Enter Marks : ', marks => {
+    console.log(`Marks :  ${marks}`);
+      readline.close(); 
+    if(marks >= 90){
+        console.log('A');
+    }else if 
+        (marks >= 80 && marks <= 89){
+            console.log('B');
+        }else if (marks >= 70 && marks <=79) {
+            console.log('C');
+        }else {
+            console.log('Fail')
+        }
+
+    }
 
 
+);
 
