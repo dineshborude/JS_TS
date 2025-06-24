00fb95a5e417fabeda44ba0dfa83892ba7262ae2
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-24 23:17:26 +0530

    functions

---
+++ Basics/functions.js
@@ -0,0 +1,30 @@
+
+function greetings() {
+
+    console.log("Hello");
+
+}
+
+greetings();
+
+//Parameter Function
+
+function parameterFunction(param) {
+
+    console.log(param);
+
+}
+
+parameterFunction("I Love JS"); //Argument 
+
+function sum(a,b) {
+    console.log(a+b);
+}
+
+sum(9,9);
+
+function multiplication(x,y,z) {
+    return ans = x*y*z;
+}
+
+console.log(multiplication(2,3,4));
