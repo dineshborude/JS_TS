23ee59ebe082955dbba160b778bf2a3aa909e5b4
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-24 23:40:52 +0530

    count vowels

--- Basics/functions.js
+++ Basics/functions.js
@@ -1,7 +1,7 @@
 
 function greetings() {
 
-    console.log("Hello");
+    // console.log("Hello");
 
 }
 
@@ -11,20 +11,46 @@ greetings();
 
 function parameterFunction(param) {
 
-    console.log(param);
+    // console.log(param);
 
 }
 
 parameterFunction("I Love JS"); //Argument 
 
 function sum(a,b) {
-    console.log(a+b);
+    // console.log(a+b);
 }
 
-sum(9,9);
+// sum(9,9);
 
 function multiplication(x,y,z) {
     return ans = x*y*z;
 }
 
-console.log(multiplication(2,3,4));
+// console.log(multiplication(2,3,4));
+
+// Arrow Functions
+
+const arrowfunction = (a,b) => {
+    console.log(a*b);
+}
+
+// arrowfunction(4,6);
+
+function returnVowels(str) {
+
+    let vowels = ['a','e','i','o','u'];
+    let count = 0;
+    for(let i = 0; i <= str.length-1; i++) {
+        for(let j = 0; j <= vowels.length-1; j++) {
+
+            if(str[i] === vowels[j]) {
+                count++;
+            }
+
+        }
+    }
+    return count;
+}
+
+console.log(returnVowels("ytrt"));
