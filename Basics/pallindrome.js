e2c4c74fde9a01b0e6100fa7b57492a8289be300
Dinesh Borude <borudedinesh2@gmail.com>
2025-06-27 00:28:48 +0530

    pallindrome

---
+++ Basics/pallindrome.js
@@ -0,0 +1,18 @@
+
+function checkPallindrome(str) {
+
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    console.log(str);
+    let reverse = "";
+    for(let i = str.length-1; i >=0; i--) {
+        reverse = reverse + str[i];
+    }
+    console.log(reverse);
+    if(reverse == str) {
+        console.log('Pallindrome');
+    }
+    else {
+        console.log('Not Pallindrome');
+    }
+}
+checkPallindrome('Evil is a name of a foeman, as I live');
