09b3b6e372a7cb351bc4b0d7359b70b7a6eb8b0c
Dinesh Borude <borudedinesh2@gmail.com>
2025-07-26 12:01:17 +0530

    globals

---
+++ Basics/JS/globalVariable.js
@@ -0,0 +1,16 @@
+
+var a = 10;
+let c = 20;
+
+function b() {
+
+    var x = 100;
+    console.log(`x :  ${x}`)
+    return x;
+
+}
+
+console.log(a);
+console.log(b());
+console.log(globalThis.a)
+console.log(global.c) //undefined
