18c1f5091c658179ca42fb6798540bb043adc832
Dinesh Borude <borudedinesh2@gmail.com>
2025-12-14 21:51:51 +0530

    push

---
+++ Basics/DSA/numberSwaps.js
@@ -0,0 +1,11 @@
+//Using Third Variable
+
+let a = 10; console.log("Value Of A : " + (a));
+let b = 20; console.log("Value Of B : " + (b));
+
+let c; console.log("Value Of C : " + (c));
+
+console.log("----------------------------*--------------------------------")
+
+a = c; console.log("Value Of A : " + (a));
+
---
+++ Basics/DSA/unary.js
@@ -0,0 +1,7 @@
+let a = 11;
+let b = 22;
+let c = a + b + a++ + b++ + ++a + ++b
+
+console.log("Value Of A : " + (a));
+console.log("Value Of B : " + (b));
+console.log("Value Of C : " + (c));
