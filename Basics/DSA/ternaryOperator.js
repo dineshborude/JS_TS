55ef324b5f9e7b6409382bd6ff70634a0d9358c2
Dinesh Borude <borudedinesh2@gmail.com>
2025-12-22 23:08:01 +0530

    switch

--- Basics/DSA/ternaryOperator.js
+++ Basics/DSA/ternaryOperator.js
@@ -1,1 +1,6 @@
-console.log(1>100?"greater":"smaller");
+console.log(1>100?"greater":"smaller"); // Ternary
+
+// Nested Ternary
+
+num = 1-1;
+console.log(num > 0?"Positive":num<0?"Negative":"Zero");
