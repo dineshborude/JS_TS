ff589d7b1f66902dfb276b41a646744780b6ac04
Dinesh Borude <borudedinesh2@gmail.com>
2025-12-22 23:37:05 +0530

    precison check

--- Basics/DSA/switch.js
+++ Basics/DSA/switch.js
@@ -1,3 +1,4 @@
+/*
 let day = 2;
 
 switch(day) {
@@ -8,4 +9,29 @@ switch(day) {
     break;
 
     default: console.log("Invalid");
+}
+
+let number = "Two";
+
+switch(number) {
+    case "One" : console.log("1");
+    break;
+    case "Two" : console.log("2");
+    break;
+}
+*/
+// Precision Handling
+
+let sum = 0.1 + 0.2;
+
+console.log(sum);
+
+
+switch(Number(sum.toPrecision(1))) {
+    case 0.3: console.log("Its your boy Paadshaah");
+    break;
+    case 0.4: console.log("Yo Yo");
+    break;
+
+    default : console.log("Raaaaaaftaaaaaaar");
 }
