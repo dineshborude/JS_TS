e8a7f600b04a4381f746644e94bbd32db50ce7b2
Dinesh Borude <borudedinesh2@gmail.com>
2025-12-25 23:35:13 +0530

    sum of number

---
+++ Basics/DSA/sumOfNumber.js
@@ -0,0 +1,14 @@
+let number = 1234;
+let sum = 0;
+
+while(number > 0) {
+
+    let reminder = number%10; // 4
+    sum += reminder;
+
+    number = Math.floor(number/10);
+   
+
+}
+
+console.log(sum);
