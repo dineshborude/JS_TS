d0f4246412bf2e41560d5d98bb4f5bf9ee38edf5
Dinesh Borude <borudedinesh2@gmail.com>
2025-12-26 00:27:49 +0530

    Strong Number Done

--- Basics/DSA/strongNumber.js
+++ Basics/DSA/strongNumber.js
@@ -1,15 +1,21 @@
-let number = 146;
+let number = 40585;
 let copy = number;
+let sum = 0;
 
-while (number > 0) {
+while(number > 0) {
 
     let reminder = number % 10;
-    let sum = 0;
+    let factorial = 1;
 
     for(let i = 1; i <= reminder; i++) {
 
-        sum += reminder*i;
+        factorial = factorial * i; 
 
     }
 
+    sum = sum + factorial;
+    number = Math.floor(number/10);
+
 }
+
+console.log((sum === copy ? "Strong Number" : "Not Strong Number"));
