4fe2b051ec8cf933eef10e6c23a43c3c7e9585b2
Dinesh Borude <borudedinesh2@gmail.com>
2026-01-06 23:55:54 +0530

    string character

---
+++ Basics/DSA/Loops_Patterns/countChars.js
@@ -0,0 +1,9 @@
+let count = 0;
+let str = "Dinesh";
+console.log(str.length);
+
+// for(let i = 0; i <= str.length(); i++) {
+
+
+
+// }
--- Basics/DSA/Loops_Patterns/rightAngleNumbers.js
+++ Basics/DSA/Loops_Patterns/rightAngleNumbers.js
@@ -1,10 +1,10 @@
-let num = 5;
+let num = 1;
 
-for(let i = 1; i <= num; i++) {
+for(let i = 5; i >= num; i--) {
 
     for(let j = 1; j <= i; j++) {
 
-         process.stdout.write(`${j}`);
+         process.stdout.write(`${j + " "}`);
 
 
     }
